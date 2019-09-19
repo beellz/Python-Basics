5eda55c7501b43ebfa6aaff2e086143f129b8daa
beellz <belal20795@gmail.com>
2019-09-19 17:38:03 +0530

    adding conditional

---
+++ Conditons.py
@@ -0,0 +1,13 @@
+print("we are going to look for greater or lower or equal number ")
+
+x = int(input("X: "))
+y = int(input("Y: "))
+
+if x < y:
+    print("x is less than Y")
+elif x > y:
+    print("X is greter than Y")
+else:
+    print("X is equal to Y")
+
+print("Thank You For playing")
