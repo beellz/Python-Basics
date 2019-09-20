c6446acba46029e19e011567dd92450a59323b0f
beellz <belal20795@gmail.com>
2019-09-20 18:07:03 +0530

    adding score with append function to make average

---
+++ score.py
@@ -0,0 +1,12 @@
+scores = []
+scores.append(72)
+scores.append(32)
+scores.append(720)
+scores.append(326)
+
+print(f"Average Sum: {sum(scores) / len(scores)}") 
+
+all = [20, 13 ,16,15,48,65,45,88,779,72,674,6345,634,64,5646,4654,65,46,44,64,645654,4,465,4654,6]
+
+print(f"Average all : {sum(all) / len(all)}") 
+
